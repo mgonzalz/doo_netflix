e9e199670cf5c738ee4d7fa001fe20fe30cb3627
mgonzalz <mgonzgarc@myuax.com>
2024-12-09 00:53:47 +0100

    configuración django rest framework

--- netflix/netflix/settings.py
+++ netflix/netflix/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'streaming',
+
+
+    # Third party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
